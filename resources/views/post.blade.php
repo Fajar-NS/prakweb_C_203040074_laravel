431243aadbd3c86ee44b3146fa100c99eaf2c316
unknown <Fajar-NS>
2022-10-17 17:08:23 +0700

    Blade Templating Engine

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    
-    <title>post</title>
-</head>
-<body>
-    <h1>post</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+
+    <article class="mb-5">
+        <h2>{{ $post['title'] }}</h2>
+        <h5>By: {{ $post['author'] }}</h5>
+        <p>{{ $post['body'] }}</p>
+    </article>
+    <a href="/posts">back to post</a>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,56 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home",
+        "active" => 'home'
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about',[
+        "title" =>"About",
         "name" => "Fajar NS",
         "kelas"=> "IF B",
         "image" => "Fajar.png"
     ]);
 });
 
-Route::get('/post', function () {
-    return view('post');
+
+Route::get('/posts', function () {
+    $blog_posts =[
+            [
+                "title"=> "judul 1",
+                "slug"=> "judul-1",
+                "author" =>"XGO",
+                "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit praesentium dolorem quae quis porro aliquam, saepe officia quisquam labore consequatur magni nam officiis necessitatibus assumenda, hic ullam. Odio deserunt libero totam ab illum iusto rem veniam nobis obcaecati similique ipsa eligendi ut quidem, in, enim corrupti! Aperiam voluptate reiciendis placeat facilis, in tempora dolore distinctio illo excepturi hic esse! Temporibus pariatur neque facilis, doloribus nobis voluptates maiores enim, eius quo, vitae libero sapiente rerum sint. Corporis, vel est? Facilis fuga ut similique deleniti numquam totam ea natus dolor dolore odio."
+            ],
+            [
+                "title"=> "judul 2",
+                "slug"=> "judul-2",
+                "author" =>"XGO",
+                "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit praesentium dolorem quae quis porro aliquam, saepe officia quisquam labore consequatur magni nam officiis necessitatibus assumenda, hic ullam. Odio deserunt libero totam ab illum iusto rem veniam nobis obcaecati similique ipsa eligendi ut quidem, in, enim corrupti! Aperiam voluptate reiciendis placeat facilis, in tempora dolore distinctio illo excepturi hic esse! Temporibus pariatur neque facilis, doloribus nobis voluptates maiores enim, eius quo, vitae libero sapiente rerum sint. Corporis, vel est? Facilis fuga ut similique deleniti numquam totam ea natus dolor dolore odio."
+            ]
+        ];
+
+        $new_post = [];
+        foreach($blog_posts as $post){
+            if($post["slug"] === $slug){
+                $new_post = $post;
+            }
+        }
+
+        
+
+
+    // halaman single post
+    Route::get('posts/{slug}', function($slug){
+        return view('post',[
+            "title"=>"awalun",
+            "post"=> $new_post
+        ]);
+    });
+    
+
+
 });
