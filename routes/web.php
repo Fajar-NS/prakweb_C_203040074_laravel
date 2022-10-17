eb20fc0e0530e8df9b2b01337419328df2c941bd
unknown <Fajar-NS>
2022-10-17 15:18:53 +0700

    Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Fajar NS",
+        "kelas"=> "IF B",
+        "image" => "Fajar.png"
+    ]);
+});
+
+Route::get('/post', function () {
+    return view('post');
 });
