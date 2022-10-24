4e67466f647b5144983750ff913729d4cca0c15e
unknown <Fajar-NS>
2022-10-24 11:16:55 +0700

    Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,40 +34,7 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/posts', function () {
-    $blog_posts =[
-            [
-                "title"=> "judul 1",
-                "slug"=> "judul-1",
-                "author" =>"XGO",
-                "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit praesentium dolorem quae quis porro aliquam, saepe officia quisquam labore consequatur magni nam officiis necessitatibus assumenda, hic ullam. Odio deserunt libero totam ab illum iusto rem veniam nobis obcaecati similique ipsa eligendi ut quidem, in, enim corrupti! Aperiam voluptate reiciendis placeat facilis, in tempora dolore distinctio illo excepturi hic esse! Temporibus pariatur neque facilis, doloribus nobis voluptates maiores enim, eius quo, vitae libero sapiente rerum sint. Corporis, vel est? Facilis fuga ut similique deleniti numquam totam ea natus dolor dolore odio."
-            ],
-            [
-                "title"=> "judul 2",
-                "slug"=> "judul-2",
-                "author" =>"XGO",
-                "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit praesentium dolorem quae quis porro aliquam, saepe officia quisquam labore consequatur magni nam officiis necessitatibus assumenda, hic ullam. Odio deserunt libero totam ab illum iusto rem veniam nobis obcaecati similique ipsa eligendi ut quidem, in, enim corrupti! Aperiam voluptate reiciendis placeat facilis, in tempora dolore distinctio illo excepturi hic esse! Temporibus pariatur neque facilis, doloribus nobis voluptates maiores enim, eius quo, vitae libero sapiente rerum sint. Corporis, vel est? Facilis fuga ut similique deleniti numquam totam ea natus dolor dolore odio."
-            ]
-        ];
-
-        $new_post = [];
-        foreach($blog_posts as $post){
-            if($post["slug"] === $slug){
-                $new_post = $post;
-            }
-        }
-
-        
-
-
-    // halaman single post
-    Route::get('posts/{slug}', function($slug){
-        return view('post',[
-            "title"=>"awalun",
-            "post"=> $new_post
-        ]);
-    });
+Route::get('/posts', [PostController::class,'index']);
+Route::get('/posts/{slug}', [PostController::class,'show']);
     
 
-
-});
